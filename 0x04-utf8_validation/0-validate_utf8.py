506982c11215f553ebb163689cc2a97757414a8e
bossclement <bosstclement@gmail.com>
2024-08-23 00:08:49 +0200

    New changes

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+utf 8 validation:
+"""
+
+
+def validUTF8(data):
+    """_summary_
+
+    Args:
+            data (list[int]): array of integers
+    """
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
